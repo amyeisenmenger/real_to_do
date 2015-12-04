b3e7b44b7ea029146e4b77ce9791fe05c3b6305e
Amy Eisenmenger <amy.eisenmenger@gmail.com>
2015-12-03 22:03:07 -0700

    list_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,18 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new(item_params)
+    @item = Item.new
     @list = List.find(params[:id])
   end
 
   def create
     @item = Item.new(item_params)
+    # @item[:list_id] = @list.id
+
+    if @item.save
+      binding.pry
+      redirect_to item_path(params[:list_id])
+    end
   end
 
   def edit
@@ -26,6 +32,7 @@ class ItemsController < ApplicationController
   private
 
     def item_params
+      params.require(:item).permit(:name, :description, :priority, :complete, :list_id)
     end
 
 end
